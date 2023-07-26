c09770f8e71c6a5c280dd692723e7be4898e92f5
KYakut35 <kyakut35@gmail.com>
2023-07-26 12:52:05 +0300

    footballer class fields added

--- Footballer.java
+++ Footballer.java
@@ -1,2 +1,13 @@
 public class Footballer {
+
+    private String fName;
+    private int fAge;
+    private String fPosition;
+    private String fTier;
+    private double fFee;
+    private double fSalary;
+
+
+
+
 }
