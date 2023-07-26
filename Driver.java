600c64548f01007deca11d391cbea230edd928e9
erenbadur <1erenbadur@gmail.com>
2023-07-26 13:25:02 +0300

    Driver class is modified(the menu is complete)

--- Driver.java
+++ Driver.java
@@ -2,25 +2,34 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-        choice = scanner.nextInt();
 
-        System.out.println("Welcome to Football Simulator");
-        System.out.println("Select game mode: ");
+        System.out.println("Welcome to Football Simulator 23.4!");
+        System.out.println("Please select game mode: ");
         System.out.println("1. Single player");
         System.out.println("2. Multiplayer");
         System.out.println("3. Quit");
 
-        switch(choice)
+        while(true)
         {
-            case 1:
+            Scanner scanner = new Scanner(System.in);
+            int choice;
+            choice = scanner.nextInt();
 
-            case 2:
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("This game mode is currently unavailable. Please try again later.");
+                    System.out.println("Please select game mode: ");
+                    break;
 
-            case 3:
-                scanner.close();
-                System.exit(0);
+                case 2:
+                    System.out.println("Multiplayer game is starting...");
+                    break;
+                case 3:
+                    System.out.println("Hope to see you again soon");
+                    scanner.close();
+                    System.exit(0);
+            }
         }
     }
 }
