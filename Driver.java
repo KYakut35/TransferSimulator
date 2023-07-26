d95241aee058a0b420daeee1720157430e78c4e4
erenbadur <1erenbadur@gmail.com>
2023-07-26 13:05:34 +0300

    Driver class is formed

--- Driver.java
+++ Driver.java
@@ -1,7 +1,26 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
-        Scanner scanner = new
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        choice = scanner.nextInt();
 
+        System.out.println("Welcome to Football Simulator");
+        System.out.println("Select game mode: ");
+        System.out.println("1. Single player");
+        System.out.println("2. Multiplayer");
+        System.out.println("3. Quit");
+
+        switch(choice)
+        {
+            case 1:
+
+            case 2:
+
+            case 3:
+                scanner.close();
+                System.exit(0);
+        }
     }
 }
